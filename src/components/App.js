cf7920e8a96c881f8a062919d9984045f10c1687
ignacioserrano9 <serranoi0910@gmail.com>
2020-07-15 20:06:34 +0200

    done, fucking bootstrap

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import CountriesList from './CountriesList';
+import {Route} from 'react-router-dom';
+import CountryDetail from './CountryDetail'
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-dark bg-primary mb-3">
+                <div className="container">
+                    <a className="navbar-brand" href="#">WIKICOUNTRIES</a>
+                </div>
+            </nav>
+                <CountriesList />
+                <div className = "col-7">
+                <Route exact path="/countries/:id" component = {CountryDetail} />
+                </div>
+            </div>
+        )
+    }
+}
